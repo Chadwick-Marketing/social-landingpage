8545dbf8afba2a72be1fc93a8b0b030c2cecdbd2
colin-chadwick <62803839+colin-chadwick@users.noreply.github.com>
2022-09-14 20:17:13 +0200

    1 file modified

--- pages/index.js
+++ pages/index.js
@@ -399,7 +399,7 @@ export default function Home() {
             </div>
             <div className="md:w-6/12 py-20">
               <h2 className="lg:leading-none text-5xl md:block  text-slate-900 font-bold pt-10 md:pt-0 whitespace-pre-line font-serif">
-                Gain meaningful stats,
+                Get meaningful stats,
                 <div className="relative text-[#4353FF] mr-2 inline w-fit">
                   {' '}
                   in realtime.
