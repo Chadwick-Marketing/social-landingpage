0401da544d83b442cbb4f6872da716a1f998dfe8
Colin Chadwick <mail@colinchadwick.de>
2022-10-25 17:15:12 +0200

    update components/Navigation/index.js

--- components/Navigation/index.js
+++ components/Navigation/index.js
@@ -64,7 +64,7 @@ const Navigation = ({ language, pricingInView }) => {
         </div>
       </div>
       <Transition
-     show={pricingInView}
+     show={pricingInView || false}
      appear={true}
      enter="transition ease-out duration-100"
        enterFrom="transform opacity-0 scale-95"
